4464609ec1e88c5d0bc86f7a3333e2d60f07b87f
Anouar <anouar.bouakkez@gmailcom>
2018-09-22 15:10:16 +0100

    connecting to db

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 
 const port = process.env.PORT || "5005";
 app.listen(port, () => {
   console.log(`connecting to port ${port} ...`);
 });
+
+mongoose
+  .connect(
+    "mongodb://localhost/medicin",
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("connecting to mongodb medicin ..."))
+  .catch(err => console.log(err.message));
