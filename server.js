4d05dae55b12742feba9a0df4880456c43facd40
Anouar <anouar.bouakkez@gmailcom>
2018-09-22 15:14:54 +0100

    refactoring startup db && app.listen

--- server.js
+++ server.js
@@ -1,16 +1,6 @@
+//modules
 const express = require("express");
 const app = express();
-const mongoose = require("mongoose");
-
-const port = process.env.PORT || "5005";
-app.listen(port, () => {
-  console.log(`connecting to port ${port} ...`);
-});
-
-mongoose
-  .connect(
-    "mongodb://localhost/medicin",
-    { useNewUrlParser: true }
-  )
-  .then(() => console.log("connecting to mongodb medicin ..."))
-  .catch(err => console.log(err.message));
+//startup
+require("./startup/db")(); //connecting db
+require("./startup/listen")(app); // app.listen
